441b2fd51f9348408473ca75b25a23027e6ea4e5
Seshwi <147176322+Seshwi@users.noreply.github.com>
2024-01-23 21:00:25 +0530

    Create jashu.py

---
+++ jashu.py
@@ -0,0 +1,14 @@
+import boto3
+lambda_Client = boto3.client('lambda', region_name='us-east-1')
+response =lambda_Client.create_function(
+            Code={
+                'S3Bucket': 'jashu-test',
+                'S3Key': 'test.py.zip', #how can i create or fetch this S3Key
+            },
+
+            FunctionName='testlambda',
+            Handler='index.handler',
+            Publish=True,
+            Role='arn:aws:iam::880305499190:role/lambda-ec2',
+            Runtime='python3.12',
+        )
